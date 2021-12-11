5f81cae84aef0896f7bb6060c6908b5f6d4b4ff9
Hiago Moura <hiagomouracandido@gmail.com>
2021-12-10 21:15:23 -0300

    Adicionado script Javascript e Redes Sociais

---
+++ script.js
@@ -0,0 +1,21 @@
+// var imgPng = document.getElementById("img-png");
+
+// imgPng.addEventListener('click', () =>{
+//     alert("você será direcionario a uma rede social de Hiago");
+// });
+
+var redeGithub = document.querySelector(".rede.github");
+var redeInstagram = document.querySelector(".rede.instagram");
+var redeLinkedin = document.querySelector(".rede.linkedin");
+
+redeGithub.addEventListener('click', () =>{
+    alert("Você será direcionario ao Gihub de Hiago");
+});
+
+redeInstagram.addEventListener('click', () =>{
+    alert("Você será direcionario ao Instagram de Hiago");
+});
+
+redeLinkedin.addEventListener('click', () =>{
+    alert("Você será direcionario ao Linkedin de Hiago");
+});
